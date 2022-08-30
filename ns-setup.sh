82601c60928ef7500d6552733301917b6998f6e3
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-08-30 19:48:33 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -3,8 +3,8 @@
 sudo docker run hello-world
 
 #Ставим и запускаем Portainer
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
 
 echo Итак, немного настроек
 echo
@@ -19,8 +19,8 @@ read secret
 echo "NS_SECRET=$secret" >> .env
 echo
 
-curl https://raw.githubusercontent.com/mo211073bkp/test/docker-compose.yml --output docker-compose.yml
-sudo docker-compose up -d
+curl https://raw.githubusercontent.com/mo211073bkp/test/main/docker-compose.yml --output docker-compose.yml
+docker-compose up -d
 
 echo "Ваш секретный ключ для доступа к Nightscout (запишите!):"
 echo "secret: $secret"
