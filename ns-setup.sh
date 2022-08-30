4478f1d4504f6d28e07e37cfbc58653d444929da
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-08-30 19:45:50 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -2,6 +2,10 @@
 
 sudo docker run hello-world
 
+#Ставим и запускаем Portainer
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
+
 echo Итак, немного настроек
 echo
 echo Введите имя пациента:
