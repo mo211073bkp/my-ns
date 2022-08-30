4f32bffa0f525a9d4f6c8040878e17cb8804d718
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-08-30 20:36:00 +0300

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -6,6 +6,7 @@ sudo systemctl status docker
 #Ставим и запускаем Portainer
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
+docker ps
 
 echo Итак, немного настроек
 echo
