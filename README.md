b5cbf523a53817e0585f7d76e9bde0207a3ca900
mo211073bkp <46835974+mo211073bkp@users.noreply.github.com>
2022-09-02 20:32:14 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# ns-setup
+Установка nightscout на VPS jino.ru
+
+Тут будет какая-то инструкция (а может и не будет)
+
+## Prerequisites
+1. Вы уже зарегистрировали VPS на jino.ru.
+2. Вы уже зарегистрировали домен и привязали его к этой VPS.
+
+## Process
+1. Зайт в консоль своей VPS
+2. вставить строку: `bash <(wget -qO- https://raw.githubusercontent.com/mo211073bkp/ns-justmara/mine/ns-setup.sh)` нажать enter и следовать инструкциям
+3. ура, у вас есть найтскаут
---
+++ ns-setup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+sudo apt-get update
+
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker run hello-world
+sudo systemctl status docker
+
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
+sudo docker ps
+
+echo Итак, немного настроек
+echo
+echo Введите имя домена, на котором ваш Nightscout будет доступен:
+read domain
+echo "NS_DOMAIN=$domain" >> .env
+echo
+echo
+echo Придумайте и введите APIsecret ключ, 12 символов буквы и цифры:
+read secret
+echo echo "NS_SECRET=$secret" >> .env
+echo
+echo
+echo Введите APIsecret ключ, 12 символов буквы и цифры:
+read title
+echo echo "NS_TITLE=$title" >> .env
+echo
+
+
+
+
+curl https://raw.githubusercontent.com/mo211073bkp/ns-justmara/mine/docker-compose.yml --output docker-compose.yml
+
+sudo docker compose up -d
+
+echo "Ваш секретный ключ для доступа к Nightscout (запишите!):"
+echo "secret: $secret"
+echo "Адрес Вашего Nightscout (запишите!):"
+echo "domain: $domain"
